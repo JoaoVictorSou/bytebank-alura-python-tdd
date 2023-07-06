e0acd1ee40f17ba4b610c00778349f0229ef369d
JoaoVictorSou <soulima.joao@gmail.com>
2023-07-06 17:41:27 -0300

    feat: test function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from bytebank import Funcionario
 
-funcionario_lucas = Funcionario("Lucas Oliveira", '01/04/2002', 5000)
+def age_test():
+    employee = Funcionario("Lucas Oliveira", '01/09/2002', 5000)
 
-print(funcionario_lucas)
-print(funcionario_lucas.idade)
+    print(f'Teste = {employee.idade}')
+
+age_test()
