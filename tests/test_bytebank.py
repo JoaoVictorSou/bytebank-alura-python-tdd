d01bbde224b04c809d634bf53ccfab674ea071ab
JoaoVictorSou <soulima.joao@gmail.com>
2023-07-17 21:32:26 -0300

    refactor: dynamic age test

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -2,10 +2,16 @@ from datetime import datetime
 from src.bytebank import Funcionario
 
 class TestClass:
-    def test_age_to_2000_03_13_date_should_return_23(self):
+    def test_age_to_2000_03_13_date_should_return_correct_years_old(self):
         # Given
         entry = '13/03/2000'
+        current_date = datetime.now()
         expect = 23
+        
+        if current_date.month < 3:
+            expect -= 1
+        elif current_date.day < 13:
+            expect -= 1
 
         # When
         employee = Funcionario('João', entry, 2000)
